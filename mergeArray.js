5f37e7235c1841fe4f21ee1bb03e5df6b7cd2397
Goutam Chandra Gharami <dev.goutam05@gmail.com>
2025-06-14 22:18:59 +0600

    Create mergeArray.js

---
+++ mergeArray.js
@@ -0,0 +1,7 @@
+// Function to merge two arrays
+function mergeArrays(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
+
+// Example usage:
+console.log(mergeArrays([1, 2], [3, 4])); // Output: [1, 2, 3, 4]
